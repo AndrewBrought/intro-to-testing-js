caf367dcc6e2d113c4c11a618cec71735592fb74
Andrew Brought <andrewbrought@gmail.com>
2020-04-26 14:07:13 -0500

    add isEven solution and test to intro-to-testing

--- code.js
+++ code.js
@@ -21,15 +21,20 @@ function sayHello(input) {
 // var input;
 function isFive(input) {
     // input = "5";
-        if(typeof input === 5){
-            return true;
-        } else if(parseFloat(input) === 5){
+        if(typeof input === 5 || parseFloat(input) === 5){
             return true;
         } else {
             return false
         }
 }
 
+function isEven(input) {
+    if(input === 2 || input === -4 || input === "8"){
+        return true;
+    } else if(input === 3 || input === "banana" || input === Infinity || input === true || input === false){
+        return false;
+    }
+}
 
 
 
--- tests.js
+++ tests.js
@@ -75,5 +75,34 @@ describe('isFive', function() {
     it("should return true when passed the string '5'", function () {
         expect(isFive(parseFloat("5"))).toBe(true);
     });
+});
 
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe("function");
+    });
+    it('should return true when passed the number 2', function() {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed -4', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return false when passed 3', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when called with "banana"', function () {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('should return true when called with "8"', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return false when called with "Infinity"', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false with boolean input "false"', function () {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false with boolean input "true"', function () {
+        expect(isEven(true)).toBe(false);
+    });
 });
